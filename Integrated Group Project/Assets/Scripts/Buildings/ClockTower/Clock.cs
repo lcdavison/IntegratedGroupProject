f5c09c0c6502bcde4337c4abfe3e62d1a3125146
lcdavison <luke.davison@talktalk.net>
2019-04-24 21:49:56 +0100

    Changed to drag and drop clocks

--- Integrated Group Project/Assets/Scripts/Buildings/ClockTower/Clock.cs
+++ Integrated Group Project/Assets/Scripts/Buildings/ClockTower/Clock.cs
@@ -4,5 +4,33 @@ using UnityEngine;
 
 public class Clock : MonoBehaviour
 {
+    private bool dragging = false;
+    private RectTransform r_transform;
+
     public Vector2 time;
+
+    void Start ( )
+    {
+        r_transform = gameObject.GetComponent <RectTransform> ( );
+    }
+
+    void Update ( )
+    {
+        if ( dragging )
+        {
+            r_transform.anchoredPosition = new Vector2 ( Input.mousePosition.x - Screen.width * 0.5f, Input.mousePosition.y - Screen.height * 0.5f );
+        }
+    }
+
+    public void OnBeginDrag ( )
+    {
+        Debug.Log ( "Start Clock Drag" );
+        dragging = true;
+    }
+
+    public void OnEndDrag ( )
+    {
+        Debug.Log ( "End Clock Drag" );
+        dragging = false;
+    }
 }

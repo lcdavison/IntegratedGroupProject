4fccd17ea076fc4b241b2774630472f84f3b1d8f
lcdavison <luke.davison@talktalk.net>
2019-04-27 17:27:35 +0100

    Added coin display to the HUD

---
+++ Integrated Group Project/Assets/Scripts/Player/PlayerHUD.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHUD : MonoBehaviour
+{
+    [SerializeField]
+    private Text coin_text;
+    private int last_value = -1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        int coins = GameManager.GetCoins ( );
+        if ( coins != last_value )
+        {
+            coin_text.text = "Coins : " + coins;
+            last_value = coins;
+        }
+    }
+}

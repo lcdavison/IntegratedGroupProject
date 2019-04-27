daa8f300db51978891a77d7573b3460fb4537538
lcdavison <luke.davison@talktalk.net>
2019-04-27 18:48:11 +0100

    Added typewriter for animating introductory story

---
+++ Integrated Group Project/Assets/Scripts/Introduction/TypeWriter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TypeWriter : MonoBehaviour
+{
+    [SerializeField]
+    private Text story_output;
+
+    [SerializeField]
+    [Multiline]
+    private string introduction;
+
+    private string [ ] words;
+    private int word_marker = 0;
+    private float last_time = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        words = introduction.Split ( ' ' );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float current_time = Time.time;
+
+        if ( current_time - last_time > 0.5 && word_marker < words.Length )
+        {
+            story_output.text += words [ word_marker++ ] + " ";
+            last_time = current_time;
+        }
+    }
+}

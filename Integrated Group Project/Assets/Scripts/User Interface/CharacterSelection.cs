5323d21ff5259f6a8e258e35efcf6b60eb398258
lcdavison <luke.davison@talktalk.net>
2019-04-29 14:39:32 +0100

    Character selection screen

---
+++ Integrated Group Project/Assets/Scripts/User Interface/CharacterSelection.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CharacterSelection : MonoBehaviour
+{
+    public void OnClickMale ( )
+    {
+        GameManager.LoadSpriteAtlas ( GameManager.Gender.MALE );
+        LoadStory ( );
+    }
+
+    public void OnClickFemale ( )
+    {
+        GameManager.LoadSpriteAtlas ( GameManager.Gender.FEMALE );
+        LoadStory ( );
+    }
+
+    void LoadStory ( )
+    {
+        SceneManager.LoadScene ( "StoryIntro" );
+    }
+}

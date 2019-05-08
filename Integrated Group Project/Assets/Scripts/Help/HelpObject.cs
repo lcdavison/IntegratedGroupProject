f380245733fb098f610ab2cbbbbd8af944207afd
lcdavison <luke.davison@talktalk.net>
2019-05-08 19:26:44 +0100

    Added help menus

---
+++ Integrated Group Project/Assets/Scripts/Help/HelpObject.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (menuName = "Help/HelpInfo", fileName = "help")]
+public class HelpObject : ScriptableObject
+{
+    [Multiline]
+    public string help_information;
+}
---
+++ Integrated Group Project/Assets/Scripts/User Interface/Help/HelpScreen.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HelpScreen : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject to_hide;
+
+    [SerializeField]
+    private GameObject help_screen;
+
+    [SerializeField]
+    private Text help_output;
+
+    [SerializeField]
+    private HelpObject help_text;
+
+    public void OnHelpClick ( )
+    {
+        to_hide.SetActive ( false );
+        help_screen.SetActive ( true );
+
+        help_output.text = help_text.help_information;
+    }
+
+    public void OnHelpExitClick ( )
+    {
+        to_hide.SetActive ( true );
+        help_screen.SetActive ( false );
+    }
+}
